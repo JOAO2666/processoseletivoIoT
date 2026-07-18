5b23bd4560166fb6e3596b3907091168002404b9
JOAO2666 <joao@example.com>
2026-07-18 16:02:07 -0300

    fix: Restaurar time.sleep(0.1) para evitar boot timeout do CI

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ def ler_temperatura():
 
 # Inicializacao
 inicializar_mpu()
-time.sleep(1.5) # Pausa maior para garantir que o CI conecte na Serial
+time.sleep(0.1) # Breve pausa para estabilizacao
 print("Sistema de Monitoramento Inicializado")
 
 # Variaveis de Estado
